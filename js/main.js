f245730d87e1c4ec587e8aa66b257aed352934d2
AnisAnsari <ansari.anis37@gmail.com>
2023-05-25 17:03:07 +0530

    all page created

--- js/main.js
+++ js/main.js
@@ -206,3 +206,45 @@ $(".project-carousel, .testimonial-carousel").owlCarousel({
     },
   },
 });
+
+// history page js 
+var items = document.querySelectorAll(".timeline li");
+
+      function isElementInViewport(el) {
+        var rect = el.getBoundingClientRect();
+        return (
+          rect.top >= 0 &&
+          rect.left >= 0 &&
+          rect.bottom <=
+            (window.innerHeight || document.documentElement.clientHeight) &&
+          rect.right <=
+            (window.innerWidth || document.documentElement.clientWidth)
+        );
+      }
+
+      function callbackFunc() {
+        for (var i = 0; i < items.length; i++) {
+          if (isElementInViewport(items[i])) {
+            if (!items[i].classList.contains("in-view")) {
+              items[i].classList.add("in-view");
+            }
+          } else if (items[i].classList.contains("in-view")) {
+            items[i].classList.remove("in-view");
+          }
+        }
+      }
+
+      window.addEventListener("load", callbackFunc);
+      window.addEventListener("scroll", callbackFunc);
+
+      // photos gallery js
+      (function () {
+        $(".img-link").magnificPopup({
+          gallery: {
+            enabled: true,
+          },
+          removalDelay: 300, // Delay in milliseconds before popup is removed
+          mainClass: "mfp-with-zoom", // this class is for CSS animation below
+          type: "image",
+        });
+      })();
